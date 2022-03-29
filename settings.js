c2e5653561346547a84060fb80d7fea199f470ee
Xtrailz <85149576+Xtrailz@users.noreply.github.com>
2022-03-29 12:17:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,18 +11,18 @@ global.APIKeys = {
 }
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'RyuBotz MD' //Ganti Jadi Nama Botmu
-global.packname = '© Ryubotz MD' //Sticker Weem
-global.author = 'Ryubotz By Ryuuka Team' //Sticker Weem
+global.namaowner = 'Hillal Zian' //Ganti Jadi Namamu
+global.owner = ['6289678423699'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6289678423699' //Owner Utama Istilahnya
+global.namabot = 'Xtrailz Bot' //Ganti Jadi Nama Botmu
+global.packname = '© Xtrailz Bot' //Sticker Weem
+global.author = 'By Zian' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
 global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
-global.youtube = 'https://youtube.com/c/RyuukaBotz'
-global.github = 'https://github.com/R-TEAM94'
-global.instagram = 'https://instagram.com/fandyyy._'
+global.youtube = 'https://youtube.com/channel/UCZjPStjZEJV_2H4moucLP_w'
+global.github = 'https://github.com/Xtrailz'
+global.instagram = 'https://instagram.com/x.trailz'
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -35,14 +35,14 @@ global.sp = '⭔'
 
 //===> EDIT SESUAI KEMAUANMU
 global.mess = {
-admin: '*LU ADMIN? 🤨*',
-botAdmin: '*BOT BUKAN ADMIN 😔*',
-owner: '*LU BUKAN OWNER GW 😠*',
-group: '*KHUSUS GROUP 😏*',
-private: '*KHUSUS PRIVATE CHAT*',
+admin: '*Anda bukan admin*',
+botAdmin: '*Bot bukan admin*',
+owner: '*Anda bukan owner bot ini*',
+group: '*Khusus grup*',
+private: '*Khusus private chat*',
 bot: '*KHUSUS PENGGUNA NOMER BOT*',
 wait: '*SABAR SEDANG DI PROSES ...*',
-done: '*DONE NGAB 🤗*',
+done: '*DONE*',
 }
 global.thumb = fs.readFileSync('./media/ryuu.jpg')
     
